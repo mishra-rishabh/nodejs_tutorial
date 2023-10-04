c19f68fa8dba9a1aed95d46cbd268c2e76926601
mishra-rishabh <rishabh.ramg@gmail.com>
2023-10-04 11:33:57 +0530

    reading file

--- 05_file_system/readFile.js
+++ 05_file_system/readFile.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const filePath = "D:/Rishabh/nodejs_tutorial/05_file_system/files/sample.txt";
+const fsPromise = require("fs").promises;
 
-// read from a file: asynchronous way
+// 1. read from a file: asynchronous way
 fs.readFile(filePath, (err, data) => {
     if(err) {
         throw new Error("Error while reading the file!");
@@ -25,8 +26,9 @@ fs.readFile(filePath, "utf-8", (err, data) => {
     console.log(data);
 });
 
-// reading file in synchronous way
+// 2. reading file in synchronous way
 try {
+    // we can also filePath here if we don't want to use path.join
     // const fileData = fs.readFileSync(path.join(__dirname, "files", "sample.txt"));
     // here also we can add character encoding 
     const fileData = fs.readFileSync(path.join(__dirname, "files", "sample.txt"), "utf-8");
@@ -35,3 +37,16 @@ try {
 } catch (error) {
     console.log(error);
 }
+
+// 3. reading file through promises
+const readFilePromiseWay = async () => {
+    try {
+        // we can either use filePath or path.join like above
+        const data = await fsPromise.readFile(filePath, {encoding: "utf-8"});
+        console.log("readFilePromiseWay: ", data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+readFilePromiseWay();
